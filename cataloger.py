ae76b9267b7561499988cdc6e8d97be5e8090915
Grant Gumina <grantgumina@Grants-MacBook-Pro-2.local>
2016-12-07 13:09:51 -0800

    Playing around with data download

--- cataloger.py
+++ cataloger.py
@@ -1,8 +1,29 @@
 import pg8000
 import json
 from eventregistry import *
+import time
 
-er = EventRegistry()
+#er = EventRegistry()
+er = EventRegistry("http://eventregistry.org", verboseOutput = True)
+q = QueryArticles()
+
+# print er.getNewsSourceUri("New York Times")
+# print er.getNewsSourceUri("Breitbart")
+
+# # articles published on 2016-03-22 or 2016-03-23
+# q.setDateLimit(datetime.date(2016, 03, 22), datetime.date(2016, 03, 23))
+
+# # published by New York Times
+# q.addNewsSource(er.getNewsSourceUri("New York Times"))
+q.addNewsSource(er.getNewsSourceUri("Breitbart"))
+
+# # return details about the articles
+q.addRequestedResult(RequestArticlesInfo(count = 5,
+    returnInfo = ReturnInfo(
+        articleInfo = ArticleInfoFlags(duplicateList = False, concepts = False, categories = False, location = False, image = False))))
+# # execute the query
+res = er.execQuery(q)
+print res
 
 # output = open('output.txt', 'w')
 # output.write(str(ret))
@@ -16,27 +37,3 @@ er = EventRegistry()
 # connection = pg8000.connect(user=username, password=password)
 # cursor = connection.cursor()
 #
-
-
-# er = EventRegistry()
-#
-# q = QueryArticles()
-# # articles published on 2016-03-22 or 2016-03-23
-# q.setDateLimit(datetime.date(2016, 03, 22), datetime.date(2016, 03, 23))
-# # related to Brussels
-# q.addConcept(er.getConceptUri("Brussels"))
-# # published by New York Times
-# q.addNewsSource(er.getNewsSourceUri("New York Times"))
-# # return details about the articles
-# q.addRequestedResult(RequestArticlesInfo(count = 30,
-#     returnInfo = ReturnInfo(
-#         articleInfo = ArticleInfoFlags(duplicateList = True, concepts = True, categories = True, location = True, image = True))))
-# # execute the query
-# res = er.execQuery(q)
-#
-#
-# # get recent articles about Obama
-# q = QueryArticles()
-# q.addConcept(er.getConceptUri("Obama"))
-# q.addRequestedResult(RequestArticlesRecentActivity())     # get most recently added articles related to obama
-# res = er.execQuery(q)
