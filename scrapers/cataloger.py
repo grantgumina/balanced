9e14fb0851bf9855a7e960630b09fedc6a309355
Grant Gumina <grantgumina@Grants-MacBook-Pro-2.local>
2016-12-13 15:59:42 -0800

    fixing scraper bugs

--- scrapers/cataloger.py
+++ scrapers/cataloger.py
@@ -6,7 +6,7 @@ import json
 import time
 import sys
 
-number_of_articles_to_retrieve = 20
+number_of_articles_to_retrieve = 50
 
 creds_file = open('creds.txt', 'r')
 lines = creds_file.read().splitlines()
@@ -20,7 +20,7 @@ connection = psycopg2.connect(database_connection_string)
 cursor = connection.cursor()
 er = EventRegistry()
 
-conservative_news_sources = ["Breitbart", "National Review Online", "The Blaze", "Daily Caller", "Washington Examiner", "Fox News"]
+conservative_news_sources = ["Washington Examiner", "Fox News"] #"Breitbart", "National Review Online", "The Blaze", "Daily Caller",
 right_leaning_news_sources = ["The Wall Street Journal", "The Economist"]
 moderate_news_sources = ["Forbes"]
 left_leaning_news_sources = ["CNN", "New York Times", "The Washington Post", "NBC News", "ABC News", "CBS News", "Reuters", "Bloomberg", "USA Today"]
@@ -31,6 +31,7 @@ news_sources = conservative_news_sources + right_leaning_news_sources + moderate
 
 for ns in news_sources:
     q = QueryArticles()
+    q.setDateLimit(datetime.date(2016, 12, 12), datetime.date(2016, 12, 13))
     q.addNewsSource(er.getNewsSourceUri(ns))
 
     # Get some articles from each news soruce
@@ -39,8 +40,7 @@ for ns in news_sources:
     # Get articles from this news source
     res = er.execQuery(q)
 
-    if res is not None and res['articles'] is not None \
-    and res['articles']['results'] is not None:
+    if res is not None and 'articles' in res and 'results' in res['articles']:
 
         for r in res['articles']['results']:
             for key, value in r.items():
@@ -56,7 +56,7 @@ for ns in news_sources:
             # Insert article into database
             sql_query_string = "INSERT INTO articles (title, body, url, uri, event_uri, date, source_name, source_url, source_id) VALUES %s RETURNING id"
             cursor.execute(sql_query_string, (values_tuple,))
-            print("Succesfully inserted article")
+            print("Succesfully inserted article: ", values_tuple)
 
             # Insert keywords into database
             high_score_concepts = []
